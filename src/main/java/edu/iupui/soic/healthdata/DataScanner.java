a5266abe2f82f19cd9b6004a342ac3e010de9eb1
Manika Maheshwari <manika.maheshwari@gmail.com>
2015-04-24 15:23:03 -0400

    fixed Java SSL exception

--- src/main/java/edu/iupui/soic/healthdata/DataScanner.java
+++ src/main/java/edu/iupui/soic/healthdata/DataScanner.java
@@ -6,11 +6,14 @@ import java.io.FileWriter;
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.ssl.SSLContext;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -23,51 +26,62 @@ import org.json.JSONObject;
  * @author Manika
  */
 public class DataScanner {
-    
+
     final String ALL_DATA_URL = "http://hub.healthdata.gov/api/2/rest/dataset";
     static String mimeType = new String();
-    
+
     public DataScanner() {
         try {
             String data = getData(ALL_DATA_URL);
-            JSONArray json = new JSONArray(data);
-            for (int i = 0; i < json.length(); i++) {
-                String uid = json.getString(i);
-                String dataDesc = getData(ALL_DATA_URL + "/" + uid);
-                System.out.println("=============================");
-                System.out.println("DATA DESC = " + dataDesc);
-                JSONObject obj = new JSONObject(dataDesc);
-                System.out.println("TITLE = " + obj.get("title") + "DESCRIPTION = " + obj.get("notes"));
-                
-                JSONArray jsonArray = obj.getJSONArray("resources");
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                String str = getData(jsonObject.getString("url"));
-                System.out.println("data= " + str);
-                if (this.mimeType.equals("text/csv") || this.mimeType.equals("text/plain")) {
-                    writeToFile(str, new File("D:\\dataFile" + i + ".txt"));
+            if (!data.equals("")) {
+                JSONArray json = new JSONArray(data);
+                for (int i = 0; i < json.length(); i++) {
+                    String uid = json.getString(i);
+                    String dataDesc = getData(ALL_DATA_URL + "/" + uid);
+                    System.out.println("=============================");
+                    System.out.println("DATA DESC = " + dataDesc);
+                    JSONObject obj = new JSONObject(dataDesc);
+                    System.out.println("TITLE = " + obj.get("title") + "DESCRIPTION = " + obj.get("notes"));
+
+                    JSONArray jsonArray = obj.getJSONArray("resources");
+                    if (jsonArray != null && jsonArray.length() > 0) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(0);
+                        String str = getData(jsonObject.getString("url"));
+
+                        System.out.println("data= " + str);
+
+                        if (this.mimeType.equals("text/csv") || this.mimeType.equals("text/plain")) {
+                            writeToFile(str, new File("D:\\dataFile" + i + ".csv"));
+                        }
+                        System.out.println("=============================");
+                    }
                 }
-                System.out.println("=============================");
             }
         } catch (IOException ex) {
             Logger.getLogger(DataScanner.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
     public final static void main(String[] args) throws Exception {
         DataScanner ds = new DataScanner();
     }
-    
+
     private String getData(String url) throws IOException {
+        SSLContext sslContext = null;
         CloseableHttpClient httpclient = HttpClients.createDefault();
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+        CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+
         HttpGet httpget = new HttpGet(url);
-        
+
         System.out.println("URL = " + url);
         System.out.println("Executing request " + httpget.getRequestLine());
 
         // Create a custom response handler
         ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
-            
+
             @Override
             public String handleResponse(final HttpResponse response) throws ClientProtocolException, IOException {
                 int status = response.getStatusLine().getStatusCode();
@@ -80,21 +94,22 @@ public class DataScanner {
                     DataScanner.mimeType = contentType.getMimeType();
                     return entity != null ? EntityUtils.toString(entity) : null;
                 } else {
-                    throw new ClientProtocolException("Unexpected response status: " + status);
+                    return "";
+                    //throw new ClientProtocolException("Unexpected response status: " + status);
                 }
             }
         };
         String responseBody = httpclient.execute(httpget, responseHandler);
         return responseBody;
     }
-    
+
     private void writeToFile(String str, File file) throws IOException {
         if (!file.exists()) {
             file.createNewFile();
             file.getParentFile().mkdirs();
-            
+
         }
-        
+
         BufferedWriter bw = new BufferedWriter(new FileWriter(file));
         bw.write(str);
         bw.close();
